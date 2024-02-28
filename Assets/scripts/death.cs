5cf66f781565e80cd78596942058182adcf74e44
N1KO-G <146829154+N1KO-G@users.noreply.github.com>
2024-02-28 18:09:19 +0000

    tutoriallevel

---
+++ Assets/scripts/death.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class death : MonoBehaviour
+{
+private void OnCollisionEnter(Collision collision)
+   {
+        if(collision.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+        }
+}
+
+}
